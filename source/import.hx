d769ea10e862f50a0f123d4bf11bb0b145b94f7f
TheLagKing <dalucanogheaven12@gmail.com>
2026-03-03 01:37:27 +0800

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -16,7 +16,6 @@ import funkin.backend.BaseStage;
 import funkin.backend.Difficulty;
 import funkin.backend.Mods;
 
-import funkin.data.Controls;
 import funkin.data.ClientPrefs;
 
 import funkin.utils.CoolUtil;
